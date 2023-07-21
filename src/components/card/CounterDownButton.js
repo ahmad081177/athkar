10ee6cd3ee91a8aa50de2d37cf0901b8ca11b5fd
ahmad811 <ahmad081177@gmail.com>
2023-07-22 00:43:54 +0300

    bg color & more doaa

--- src/components/card/CounterDownButton.js
+++ src/components/card/CounterDownButton.js
@@ -1,22 +1,22 @@
 import './CounterDownButton.css'
-import React, { useState } from 'react'
+import React from 'react'
 
 const CounterDownButton = (props) => {
     //props.num, props.count
-    const [count, setCount] = useState(props.count)
-
-    const setCountDown = () => {
-        if (count > 0) setCount(count - 1)
+    const setCountDown = (e) => {
+        e.stopPropagation();
+        if (props.count > 0) props.setCount(props.count - 1)
     }
-    const onReset = () => {
-        setCount(props.count)
+    const onReset = (e) => {
+        e.stopPropagation()
+        props.setCount(props.total)
     }
     return (
 
-        <button className='bounterButton' title={count} value={count} onClick={() => { setCountDown() }} >
-            <div>عدد المرّات - {count}</div>
+        <button className='bounterButton' title={props.count} value={props.count} onClick={(e) => { setCountDown(e) }} >
+            <div>عدد المرّات - {props.count}</div>
             <div className='reset-button'>
-               <strong> <i className="bi text-success bi-arrow-counterclockwise" onClick={() => { onReset() }}></i>
+               <strong> <i className="bi text-success bi-arrow-counterclockwise" onClick={(e) => { onReset(e) }}></i>
                 <span>{props.count}</span></strong>
             </div>
         </button>
--- src/components/card/ThikirCard.js
+++ src/components/card/ThikirCard.js
@@ -1,16 +1,28 @@
 import CounterDownButton from './CounterDownButton'
 import './ThikirCard.css'
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const ThikirCard = (props) => {
     //props.id, props.data
+    const elmRef = useRef(null)
+    const [count, setCount] = useState(props.data.count)
+    
+    useEffect(() => {
+        if (count === 0){
+            elmRef.current.style.backgroundColor='#ace9ba';
+        }
+        else{
+            elmRef.current.style.backgroundColor='#f8f9fa';
+        }
+    }, [count])
+
     return (
-        <div key={props.data.id} className="card img-rounded equalizer col-a col-xs-12 col-ms-12 col-sm-12 col-md-12 equal-height text-dark bg-light mb-3 border-success" style={{ borderBottom: "1px solid transparent" }}>
+        <div key={props.data.id}  className='card img-rounded equalizer col-a col-xs-12 col-ms-12 col-sm-12 col-md-12 equal-height text-dark bg-light mb-3 border-success' style={{ borderBottom: '1px solid'}}>
             <div className="card-header thikir-card-header">
                 دعاء #: {props.data.id + 1}
-                <CounterDownButton count={props.data.count} num={props.data.id + 1} />
+                <CounterDownButton count={count} setCount={setCount} total={props.data.count} />
             </div>
-            <div className="card-body">
+            <div ref={elmRef} className="card-body">
                 <h5 className="card-title">{props.data.title}</h5>
                 <p className="card-text">{props.data.content}</p>
                 {props.data.ref && <p>{props.data.ref}</p>}
